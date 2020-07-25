d927b21ae2e41eb0ca53b6c08babf495580a9503
Avik Kar <53268442+karavik18@users.noreply.github.com>
2020-07-26 00:27:43 +0530

    Update ot_main.py

--- src/ot_main.py
+++ src/ot_main.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 from options import args_parser
-from utils import dft, ditfft, diffft, reArrange, dct, dst, wht, haar, slant, klt
+from utils import getSequence, dft, ditfft, diffft, reArrange, dct, dst, wht, haar, slant, klt
 
 
 if __name__ == '__main__':
@@ -18,26 +18,30 @@ if __name__ == '__main__':
     
     if args.sequence == 'random':
         inSequence = np.random.rand(8)
+    else:
+        inSequence = getSequence()
+    
+    
     print("\nThe input sequence is: {}".format(inSequence))
     
     if args.transform == 'dft':
         task = 'Discrete Fourier Transform'
-        outSequence = dft(inSequence)
+        outSequence = np.round(dft(inSequence), args.roundDigit)
     elif args.transform == 'ditfft':
         task = 'Fast Fourier Transform (Decimation in Time)'
-        outSequence = ditfft(inSequence)
+        outSequence = np.round(ditfft(inSequence), args.roundDigit)
     elif args.transform == 'diffft':
         task = 'Fast Fourier Transform (Decimation in Frequency)'
-        outSequence = reArrange(diffft(inSequence))
+        outSequence = np.round(reArrange(diffft(inSequence)), args.roundDigit)
     elif args.transform == 'dct':
         task = 'Discrete Cosine Transform'
-        outSequence = dct(inSequence)
+        outSequence = np.round(dct(inSequence), args.roundDigit)
     elif args.transform == 'dst':
         task = 'Discrete Sine Transform'
-        outSequence = dst(inSequence)
+        outSequence = np.round(dst(inSequence),args.roundDigit)
     elif args.transform == 'wht':
         task = 'Walsh-Hadamard Transform'
-        outSequence = wht(inSequence)
+        outSequence = np.round(wht(inSequence),args.roundDigit)
     elif args.transform == 'slant':
         task = 'Slant Transform'
         outSequence = slant(inSequence)
@@ -52,8 +56,9 @@ if __name__ == '__main__':
     print("The {} of input sequence is: {}".format(task,outSequence))
 
     # Saving the objects train_loss and local_test_accuracy:
-    path = 'C:\\Users\\User-name\\Orthogonal Transforms\\save\\'  # Define save folder path here
+    path = 'C:\\Users\\Avik\\Orthogonal Transforms\\save\\'  # Define save folder path here
     file_name = '{}.pkl'.\
         format(task)
     with open(path+file_name, 'wb') as f:
         pickle.dump([inSequence, outSequence], f)
+    
