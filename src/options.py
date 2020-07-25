00e42517811cbc4439311e2b99c34aaffd5d1853
Avik Kar <53268442+karavik18@users.noreply.github.com>
2020-07-26 00:24:41 +0530

    Update options.py

--- src/options.py
+++ src/options.py
@@ -15,5 +15,7 @@ def args_parser():
                         default='DFT', help="Name of the transform.")
     parser.add_argument('--sequence', type=str, default='random', choices = ['random', 'takeinput'],
                         help="Whether the sequence will be generated or provided.")
+    parser.add_argument('--roundDigit', type=int, default=4, choices = [0, 2, 4, 6, 8],
+                        help="Number of digits after decimel place in output.")
     args = parser.parse_args()
     return args
